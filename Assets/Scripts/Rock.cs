e9086f8a7d4542d4487b9a28e13269d3b7b24174
ALIENWARE-2\Carlo <carlogalatolo@gmail.com>
2025-02-13 18:16:13 +0100

    Fino a lezione 144

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class Rock : MonoBehaviour
 {
+	// Params
+	[SerializeField] ParticleSystem collisionParticleSystem;
+	[SerializeField] AudioSource boulderSmashAudioSource;
 	[SerializeField] float shakeGain = 2;
+	[SerializeField] float collisionCooldown = 1;
+
+	// Cache
 	CinemachineImpulseSource cinemachineImpulseSource;
+	
+	// State
+	float collisionTimer = 1;
 
 	void Awake ()
 	{
@@ -12,11 +21,37 @@ public class Rock : MonoBehaviour
 	}
 
 
+	void Update ()
+	{
+		collisionTimer += Time.deltaTime;
+	}
+
+
 	void OnCollisionEnter (Collision collision)
+	{
+		if (collisionTimer >= collisionCooldown)
+		{
+			FireCameraShake();
+			PlayEffects(collision);
+			collisionTimer = 0;
+		}
+	}
+
+
+	void FireCameraShake()
 	{
 		float distance = Vector3.Distance(this.transform.position, Camera.main.transform.position);
 		float shake = 1 / distance * shakeGain;
-		shake = Mathf.Min (shake, 1);
+		shake = Mathf.Min(shake, 1);
 		cinemachineImpulseSource.GenerateImpulse(shake);
+	}
+
+
+	void PlayEffects(Collision collision)
+	{
+		ContactPoint contactPoint = collision.contacts[0];
+		collisionParticleSystem.transform.position = contactPoint.point;
+		collisionParticleSystem.Play();
+		boulderSmashAudioSource.Play();
 	}
 }
